fe25f597a721a900ebb6eb361d2d077908a132f6
ash0419 <ash0419@naver.com>
2019-12-02 20:51:08 +0900

    191202

--- keras03_summary.py
+++ keras03_summary.py
@@ -7,21 +7,19 @@ y = np.array([1,2,3,4,5,6,7,8,9,10])
 x2 = np.array([11,12,13,14,15])
 
 model = Sequential()
-# model.add(Dense(5, input_dim=1, activation='relu'))
-# model.add(Dense(3))
-# model.add(Dense(1))
-model.add(Dense(100, input_dim=1, activation='relu'))
-model.add(Dense(80))
-model.add(Dense(60))
-model.add(Dense(40))
-model.add(Dense(20))
+model.add(Dense(5, input_dim=1, activation='relu'))
+model.add(Dense(3))
 model.add(Dense(1))
 
+model.summary()
+
+'''
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit(x,y, epochs=100, batch_size=1)
-loss, acc = model.evaluate(x, y, batch_size=1)
+model.fit(x,y, epochs=100)
+loss, acc = model.evaluate(x, y)
 print("acc : ", acc)
 print("loss : ", loss)
 
 y_predict = model.predict(x2)
 print(y_predict)
+'''
